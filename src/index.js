7b6714b3917c6dc10a9fad7819f84416d6088a74
Daniël Illouz <daniel.illouz@me.com>
2019-01-05 18:23:48 +0100

    Convert Markdown tex to a PDF file
    - Use some hardcoded Markdown text and convert it to HTML
    - Use Headless Chrome to create a PDF file from HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const showdown = require('showdown');
+const puppeteer = require('puppeteer');
+
+async function main() {
+  const converter = new showdown.Converter();
+  const text = '# Hello Markdown!\n\nThis is some text.';
+  const html = converter.makeHtml(text);
+
+  console.log(html);
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // https://github.com/GoogleChrome/puppeteer/blob/v1.11.0/docs/api.md#pagesetcontenthtml-options
+  await page.setContent(html);
+
+  // https://github.com/GoogleChrome/puppeteer/blob/v1.11.0/docs/api.md#pagepdfoptions
+  await page.pdf({
+    path: 'test.pdf',
+    format: 'A4'
+  });
+
+  await browser.close();
+}
+
+main();
